4e9a63308f2746bf3fd3a9923a5d05661d5f00d7
sebastian <nugrasebas2@gmail.com>
2026-07-19 23:10:30 -0500

    feat(routes): add lazy-loaded routes for calculadora, historial, and detalle

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () =>
+            import('./pages/calculadora/calculadora').then((m) => m.Calculadora),
+    },
+    {
+        path: 'historial',
+        loadComponent: () =>
+            import('./pages/historial/historial/historial').then((m) => m.Historial),
+    },
+    {
+        path: 'historial/:id',
+        loadComponent: () =>
+            import('./pages/historial/detalle/detalle').then((m) => m.Detalle),
+    },
+    { path: '**', redirectTo: '' },
+];
