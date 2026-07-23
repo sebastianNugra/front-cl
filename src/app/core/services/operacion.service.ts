cbae500afb7c09d2952e2363d4f49691317f504f
sebastian <nugrasebas2@gmail.com>
2026-07-22 22:59:07 -0500

    refactor(service): remove unused catchError and throwError imports

--- src/app/core/services/operacion.service.ts
+++ src/app/core/services/operacion.service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable, catchError, throwError } from 'rxjs';
+import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
 import {
     OperacionRequest,
