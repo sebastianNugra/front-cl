584284e0c80937f964ed4345a994a3870cdeeea2
sebastian <nugrasebas2@gmail.com>
2026-07-23 21:20:30 -0500

    refactor(app): replace Material icons with Unicode placeholders and remove Material imports

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,5 @@
 import { Component, effect, signal } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
@@ -11,9 +8,6 @@ import { MatIconModule } from '@angular/material/icon';
     RouterOutlet,
     RouterLink,
     RouterLinkActive,
-    MatToolbarModule,
-    MatButtonModule,
-    MatIconModule,
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss',
